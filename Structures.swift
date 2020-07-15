09a550abe93ddcf0f07a01754c7f7d05531f9e93
vladislavurumov <vladislav.urumov@gmail.com>
2020-07-15 11:46:43 +0000

    Added single die roll and three dice draw functions

--- Structures.swift
+++ Structures.swift
@@ -33,7 +33,24 @@ struct Die
         self.shotguns = shotguns
         self.sum = brains + runners + shotguns
     }
+    func roll() -> String
+    {
+        let number = UInt.random(in: 1 ... sum)
+        if(number <= brains)
+        {
+            return "🧠"
+        }
+        else if(number <= brains  + runners)
+        {
+            return "👣"
+        }
+        else
+        {
+            return "💥"
+        }
+    }
 }
+
 struct Dice
 {
     var green: UInt
@@ -45,6 +62,31 @@ struct Dice
         self.yellow = yellow
         self.red = red
     }
-    
+    mutating func draw(_ _count: UInt) -> [(Die,String)]
+    {
+        var result: [(Die,String)] = []
+        var count = _count
+        while count > 0 && green + yellow + red > 0 
+        {
+            let number = UInt.random(in: 1 ... (green + yellow + red))
+            if(number <= green)
+            {
+                result.append((Die(brains: 3, runners: 2, shotguns: 1),"green"));
+                green -= 1
+            }
+            else if(number <= green  + yellow)
+            {
+                result.append((Die(brains: 2, runners: 2, shotguns: 2),"yellow"));
+                yellow -= 1
+            }
+            else
+            {
+                result.append((Die(brains: 1, runners: 2, shotguns: 3),"red"));
+                red -= 1
+            }
+            count -= 1
+        }
+        return result
+    }
 }
 
